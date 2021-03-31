40e0a94fb52cf12a26672fc9f50873ccd8b71307
janavitar <jana.vitvarova@tul.cz>
2021-03-31 12:02:31 +0200

    Add comments to Point

--- 03ALG2_NShape/src/point/Point.java
+++ 03ALG2_NShape/src/point/Point.java
@@ -1,17 +1,17 @@
 package point;
 
 /**
- * Nový datový typ, který reprezentuje bod v 2D prostoru 
+ * Nový datový typ, který reprezentuje bod v 2D prostoru
  * @author JV
  */
 public class Point {
     //data, které si potřebujeme pamatovat pro každý objekt typu Point
-    //instanční proměnné, členské proměnné, fields
+    //instanční proměnné, členské proměnné, fields, atributy
     private double x;
     private double y;
-    
+
     private double distance; //vypočítáva se jednou
-    
+
     //konstruktory
     //prázdný konstruktor
     public Point(){
@@ -20,52 +20,53 @@ public class Point {
         //distance = calculateDistance();
         distance = 0.0;
     }
-    
+
     //přetížený konstruktor, overloaded
     public Point(double x, double y){
         this.x = x;
         this.y = y;
         distance = calculateDistance();
     }
-    
+
     //metody
     public double getDistance(){
        return distance;
     }
-    
+
     //privátní interní metoda
     private double calculateDistance(){
         //return Math.sqrt(x*x + y*y);
         return Math.hypot(x, y);
     }
-    
+
     //veřejné metody tvoří rozhraní třídy, měli by mít javadoc
     /**
      * Calculate distance from this point to the given point
      * @param p given point
-     * @return distance 
+     * @return distance
      */
+    //instanční metoda, pracuje s instančími datami this.
     public double calculateDistaceFrom(Point p){
         return Math.hypot(this.x - p.x, this.y - p.y);
     }
-    
-    //metoda, která nepracuje s datami this, může být i v jiné tříďě
+
+    //statická metoda, která nepracuje s instančními datami this, může být i v jiné tříďě
     public static double calculateDistaceBetween(Point a, Point b){
         return Math.hypot(a.x - b.x, a.y - b.y);
     }
-    
+
     /*
     public String getTextRepresentation(){
         return "x=" + x + ", y=" + y;
     }
     */
-    
-    //překrytí kódu metody zděděné z předka Object 
+
+    //překrytí kódu metody zděděné z předka Object
     @Override
     public String toString(){
         return String.format("[%.2f, %.2f]",x , y);
     }
-    
+
     //getter
     public double getX() {
         return x;
@@ -74,7 +75,7 @@ public class Point {
     public double getY() {
         return y;
     }
-    
+
     /*
     //setter
     public void setX(double x) {
